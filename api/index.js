2f6165e0c049a26f96c6523afc19314b99da02fe
dev-abhishekkr <dev.abhishekkr@gmail.com>
2024-03-19 13:30:09 +0530

    Implemented User route & Auth route for new user in db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { mongoose } from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,8 +16,13 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
